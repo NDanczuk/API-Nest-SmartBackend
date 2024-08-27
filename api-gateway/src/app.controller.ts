081ce4b4fe1435590dd303b9ae2802e42f8c14b1
NDanczuk <nicolasdanc@gmail.com>
2024-08-27 16:53:33 -0300

    Added get route to consult categories

--- api-gateway/src/app.controller.ts
+++ api-gateway/src/app.controller.ts
@@ -1,8 +1,10 @@
 import {
   Body,
   Controller,
+  Get,
   Logger,
   Post,
+  Query,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
@@ -12,6 +14,7 @@ import {
   Transport,
 } from '@nestjs/microservices';
 import { CreateCategoryDto } from './dto/category/create-category.dto';
+import { Observable } from 'rxjs';
 
 @Controller('api/v1')
 export class AppController {
@@ -31,10 +34,12 @@ export class AppController {
 
   @Post('category')
   @UsePipes(ValidationPipe)
-  async createCategory(@Body() createCategoryDto: CreateCategoryDto) {
-    return await this.clientAdminBackend.emit(
-      'create-category',
-      createCategoryDto,
-    );
+  createCategory(@Body() createCategoryDto: CreateCategoryDto) {
+    return this.clientAdminBackend.emit('create-category', createCategoryDto);
+  }
+
+  @Get('category')
+  consultCategories(@Query('categoryId') _id: string): Observable<any> {
+    return this.clientAdminBackend.send('consult-categories', _id ? _id : '');
   }
 }
