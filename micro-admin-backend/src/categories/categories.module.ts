7ee5122ca516087a489375b56e1364ed6e4a3873
NDanczuk <nicolasdanc@gmail.com>
2024-09-02 09:44:52 -0300

    Import fix

--- micro-admin-backend/src/categories/categories.module.ts
+++ micro-admin-backend/src/categories/categories.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CategorySchema } from './interfaces/category.schema';
 import { CategoriesService } from './categories.service';
-import { CategoriesController } from './app.controller';
+import { CategoriesController } from './categories.controller';
 
 @Module({
   imports: [
