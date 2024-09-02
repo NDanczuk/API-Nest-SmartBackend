144251797ca1fdd7b1918946e11bd9034d03dd7f
NDanczuk <nicolasdanc@gmail.com>
2024-09-02 10:39:28 -0300

    update player dto updated

--- api-gateway/src/players/dtos/update-player.dto.ts
+++ api-gateway/src/players/dtos/update-player.dto.ts
@@ -2,7 +2,7 @@ import { IsOptional } from 'class-validator';
 
 export class UpdatePlayerDto {
   @IsOptional()
-  category: string;
+  category?: string;
 
   @IsOptional()
   playerAvatarUrl?: string;
