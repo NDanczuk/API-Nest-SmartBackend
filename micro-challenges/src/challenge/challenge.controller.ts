74bd2be43f8a6639ed6b80327fbe8e3a63723c7e
NDanczuk <nicolasdanc@gmail.com>
2024-09-03 09:29:54 -0300

    Challenge controller added to micro-challenges

--- micro-challenges/src/challenge/challenge.controller.ts
+++ micro-challenges/src/challenge/challenge.controller.ts
@@ -1,6 +1,12 @@
 import { Controller, Logger } from '@nestjs/common';
 import { ChallengeService } from './challenge.service';
-import { Ctx, EventPattern, MessagePattern, Payload, RmqContext } from '@nestjs/microservices';
+import {
+  Ctx,
+  EventPattern,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
 import { Challenge } from './interfaces/challenge.interface';
 
 const ackErrors: string[] = ['E11000'];
@@ -36,6 +42,85 @@ export class ChallengeController {
   @MessagePattern('consult-challenges')
   async consultChallenges(
     @Payload() data: any,
-    @Ctx() context: RmqContext
-  ): Promise<
+    @Ctx() context: RmqContext,
+  ): Promise<Challenge[] | Challenge> {
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+    try {
+      const { playerId, _id } = data;
+      this.logger.log(`data: ${JSON.stringify(data)}`);
+      if (playerId) {
+        return await this.challengeService.consultOnePlayersChallenges(
+          playerId,
+        );
+      } else if (_id) {
+        return await this.challengeService.consultChallengeById(_id);
+      } else {
+        return await this.challengeService.consultAllChallenges();
+      }
+    } finally {
+      await channel.ack(originalMsg);
+    }
+  }
+
+  @EventPattern('update-challenge')
+  async updateChallenge(@Payload() data: any, @Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+    try {
+      this.logger.log(`data: ${JSON.stringify(data)}`);
+      const _id: string = data.id;
+      const challenge: Challenge = data.challenge;
+      await this.challengeService.updateChallenge(_id, challenge);
+      await channel.ack(originalMsg);
+    } catch (error) {
+      const filterAckError = ackErrors.filter((ackErrors) =>
+        error.message.includes(ackErrors),
+      );
+      if (filterAckError.length > 0) {
+        await channel.ack(originalMsg);
+      }
+    }
+  }
+
+  @EventPattern('update-challenge-match')
+  async updateChallengeMatch(@Payload() data: any, @Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+    try {
+      this.logger.log(`matchId: ${data}`);
+      const matchId: string = data.matchId;
+      const challenge: Challenge = data.challenge;
+      await this.challengeService.updateChallengeMatch(matchId, challenge);
+      await channel.ack(originalMsg);
+    } catch (error) {
+      const filterAckError = ackErrors.filter((ackErrors) =>
+        error.message.includes(ackErrors),
+      );
+      if (filterAckError.length > 0) {
+        await channel.ack(originalMsg);
+      }
+    }
+  }
+
+  @EventPattern('delete-challenge')
+  async deleteChallenge(
+    @Payload() challenge: Challenge,
+    @Ctx() context: RmqContext,
+  ) {
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+    try {
+      await this.challengeService.deleteChallenge(challenge);
+      await channel.ack(originalMsg);
+    } catch (error) {
+      this.logger.log(`error: ${JSON.stringify(error.message)}`);
+      const filterAckError = ackErrors.filter((ackErrors) =>
+        error.message.includes(ackErrors),
+      );
+      if (filterAckError.length > 0) {
+        await channel.ack(originalMsg);
+      }
+    }
+  }
 }
